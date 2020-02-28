259f391d6e6311ee06a27e1cda2654787ca9a970
LaluPrasad M <laluprasad.73515274@gmail.com>
2020-02-28 23:16:25 +0530

    Update admin.js

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
-const Candidate = require('../models/candidate'); 
 
 const AdminController = require("../controllers/admin");
 const CandidateController = require("../controllers/candidate");
